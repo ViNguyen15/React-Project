2b81d57b2fa875f95ad8de63f90aa00b3a82cb7d
Vi Nguyen <fluffywin@gmail.com>
2022-02-24 16:03:03 -0500

    typo fixes, portrait resizing, button and icon resizing

--- src/components/About.js
+++ src/components/About.js
@@ -9,10 +9,10 @@ export default function About(){
             </div>
 
             <div className="about--paragraph">
-                I am a Full Stack Developer that enjoys developing projects. 
-                I always try to find ways to automate my task and loves to figure 
-                optimal yet readable solution. I try to follow best practices and 
-                am always trying to find new things to learn.
+                I am a Full Stack Developer that enjoy developing all kind of projects. 
+                I always try to find multiple ways to automate my task and loves to figure 
+                ways to get optimal and readable solutions. I try to follow best practices 
+                and am always trying to searching for new things to learn.
             </div>
         </div>
     )
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,13 +1,14 @@
 import React from "react";
 import linkImage from "../resources/linkedin.png";
 import emailImage from "../resources/email.png";
+import portrait from "../resources/Vi.jpg"
 
 export default function Info(){
 
 
     return (
         <div className="info--stuff">
-            <img className="info--image" src="https://media-exp1.licdn.com/dms/image/C4D03AQEPG4RvUGNQtQ/profile-displayphoto-shrink_800_800/0/1605528889559?e=1651104000&v=beta&t=rKLf001UmRwho-5uUg8HaVtd-1PhhA9hpFJijvVoXec" />
+            <img className="info--image" src={portrait} />
             <div className="info--name">Vi Nguyen</div>
             <div className="info--title">Full Stack Developer</div>
             <div className="info--link">ViNguyen.website</div>
